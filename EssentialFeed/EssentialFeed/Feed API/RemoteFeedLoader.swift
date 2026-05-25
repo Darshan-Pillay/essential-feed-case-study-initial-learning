7f401235477a75b47ba2eb7eaa730bce7f8135c1
Darshan Pillay <darshan.pillay@takealot.com>
2026-05-25 21:31:18 +0200

    more remotefeedloader to production

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(
+        url: URL,
+        client: HTTPClient
+    ) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
