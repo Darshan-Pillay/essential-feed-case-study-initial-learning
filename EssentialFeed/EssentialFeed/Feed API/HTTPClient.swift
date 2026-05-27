0c12e48de28ed43c479f200fa1cfa44196d1051f
Darshan Pillay <darshan.pillay@takealot.com>
2026-05-27 21:18:08 +0200

    Delivers invalidData error on 200 HTTP response with invalid json

--- EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
+++ EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
 }
