325e7b97d1f408672a0f7ef47a5759c2bbe083e6
Darshan Pillay <darshan.pillay@takealot.com>
2026-06-01 20:07:54 +0200

    remove extra feeditems mapper definition

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -36,36 +36,12 @@ public final class RemoteFeedLoader {
             }
         }
     }
-}
-
-private class FeedItemsMapper {
-    static var OK_200: Int { return 200 }
     
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
+    private func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        if let items = try? FeedItemsMapper.map(data, response) {
+            return .success(items)
+        } else {
+            return .failure(.invalidData)
         }
-        
-        return try JSONDecoder().decode(Root.self, from: data).items.map(\.item)
-    }
-}
-
-private struct Root: Decodable {
-    let items: [Item]
-}
-
-private struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
-    
-    var item: FeedItem {
-        return FeedItem(
-            id: id,
-            description: description,
-            location: location,
-            imageURL: image
-        )
     }
 }
